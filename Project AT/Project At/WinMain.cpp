92188dea56ac377fca665a118eabb5916943d0d2
JackLynham <Jack2.Lynham@live.uwe.ac.uk>
2019-10-05 15:41:57 +0100

    Made Some Inputs

--- Project AT/Project At/WinMain.cpp
+++ Project AT/Project At/WinMain.cpp
@@ -1,13 +1,48 @@
 #include <Windows.h>
-
+#include <sstream>
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-	case WM_CLOSE:
+	case WM_CLOSE:			//Signals Message loop that we want to Exit.
 		PostQuitMessage(69);    //If Closed Send this Message
 		break;
+
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, "Respects");    //Set Window Text Good Example of what I can Do
+
+		}
+		break;
+
+	case WM_KEYUP:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, "Congrats");
+
+		}
+		break;
+
+	case WM_CHAR:  //Used for When You get Actual text Input Such as Typing in you name.
+	{
+		static std::string title;
+		title.push_back((char)wParam);
+		SetWindowText(hWnd, title.c_str());
 	}
+	break;
+
+	case WM_LBUTTONDOWN:
+	{
+		POINTS pt = MAKEPOINTS(lParam);  //Extract the points 
+		std::ostringstream oss;
+		oss << "(" << pt.x << "," << pt.y << ")";
+		SetWindowText(hWnd, oss.str().c_str()); //Put it into Window Text Shows Cords of mouse in Window
+
+	}
+	break;
+	}
+
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
@@ -50,7 +85,7 @@ int CALLBACK WinMain(
 	bool gResult;
 	while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0)
 	{
-		TranslateMessage(&msg);   //It wont Change Messagess 
+		TranslateMessage(&msg);   
 		DispatchMessage(&msg);	 //Passes Messages to Wnd Proc
 	}
 	if (gResult == 1)
